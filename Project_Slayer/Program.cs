10528c8674acfe51952f708ad43e2c55655e01d8
LJkiller <OfficialLJkiller@gmail.com>
2023-10-03 10:23:30 +0200

    Add project files.

---
+++ Project_Slayer/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Slayer {
+	class Program {
+		static void Main(string[] args) {
+			Console.WriteLine("These balls are reserved for GitHub, Slayer Project");
+		}
+	}
+}
