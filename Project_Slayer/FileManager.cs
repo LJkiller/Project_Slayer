a650715402c9e031c2b13b225ddadd214bce13b7
LJkiller <OfficialLJkiller@gmail.com>
2023-10-10 11:37:34 +0200

    Struktuerat preliminär public classes för att anropa till metoder Lättare att läsa av koden

---
+++ Project_Slayer/FileManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Slayer {
+	/// <summary>
+	/// Data class to invoke methods in order to manage files.
+	/// </summary>
+	public class FileManager {
+		public void Save() {
+
+		}
+		public void Load() {
+
+		}
+		public void DisplayAllFiles() {
+
+		}
+	}
+}
---
+++ Project_Slayer/TextManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Slayer {
+	/// <summary>
+	/// Data class to invoke methods in order to manage text.
+	/// </summary>
+	public class TextManager {
+
+		public void FileInputConversion() {
+
+		}
+	}
+}
